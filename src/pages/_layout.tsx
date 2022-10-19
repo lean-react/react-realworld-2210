51faa91cc46cca5d8e470d940fe9314c0dcda12c
Micha Buchholz <mail@lean-stack.de>
2022-10-19 11:51:10 +0200

    Styled app bar.

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -1,9 +1,57 @@
-import { Outlet } from 'react-router-dom';
+import {
+  AppBar,
+  Box,
+  Button,
+  IconButton,
+  Link,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { Link as RouterLink, Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
     <div>
-      <h1>React Realworld</h1>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              React RW
+            </Typography>
+            <Box style={{ display: 'flex' }}>
+              <Button
+                component={RouterLink}
+                to="/"
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Home
+              </Button>
+              <Button
+                component={RouterLink}
+                to="/todos"
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Todos
+              </Button>
+              <Button
+                component={RouterLink}
+                to="/about"
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                About
+              </Button>
+              <Button
+                component={RouterLink}
+                to="/"
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                Login
+              </Button>
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
       <Outlet />
     </div>
   );
