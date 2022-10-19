a4a6f106ee73b2699910060155e2ace807f4a8b4
Micha Buchholz <mail@lean-stack.de>
2022-10-19 17:19:34 +0200

    Implement simple todos app

--- src/hooks/use-resource.ts
+++ src/hooks/use-resource.ts
@@ -3,7 +3,7 @@ import { useCallback } from 'react';
 import useSWR, { mutate } from 'swr';
 
 export function useResource<T>(key: string) {
-  const { data, error } = useSWR<T[]>('articles', (key) =>
+  const { data, error } = useSWR<T[]>(key, (key) =>
     axios
       .get(`https://rube-servidor.netlify.app/api/public/${key}`)
       .then((res) => res.data)
--- src/pages/todos.tsx
+++ src/pages/todos.tsx
@@ -1,7 +1,40 @@
+import axios from 'axios';
+import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
+import { useResource } from '../hooks/use-resource';
+
+type Todo = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
 export default function TodosPage() {
+  const { register, handleSubmit, reset } = useForm<Todo>({
+    defaultValues: { title: '', completed: false },
+  });
+  const { data: todos, invalidate } = useResource<Todo>('todos');
+
+  async function createTodo(data: Todo) {
+    reset();
+    await toast.promise(
+      axios.post('https://rube-servidor.netlify.app/api/public/todos', data),
+      {
+        loading: 'Speichern ...',
+        error: 'Hoppla',
+        success: 'Es gibt wieder mehr zu tun',
+      }
+    );
+    invalidate();
+  }
+
   return (
     <div>
       <h2>Todos</h2>
+      <form onSubmit={handleSubmit(createTodo)}>
+        <input {...register('title')} />
+      </form>
+      <ul>{todos && todos.map((t) => <li key={t.id}>{t.title}</li>)}</ul>
     </div>
   );
 }
